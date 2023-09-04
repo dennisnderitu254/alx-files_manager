d746acfaf3329ec0e5ec527ef2dc4f65e1f01f6d
sagebeme <eudicas5k@gmail.com>
2023-09-04 19:33:48 +0300

    parsing the server to port 5000

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const router = require('./routes/index');
+import express from 'express';
+import router from './routes/index';
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = 5000;
 
 app.use(express.json());
-app.use(router);
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`server running on port {port}`);
+  console.log(`server running on port ${port}`);
 });
 
 export default app;
